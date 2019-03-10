3b3d65c267d74658e588e593c84d2771a7245a0a
Eugene <eugenezelenov@gmail.com>
2019-03-10 00:56:29 -0500

    Modified urls.py in order to fix static files uwsgi issue

--- fiji_site/settings.py
+++ fiji_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9u=p%_2tri4f+i+^ldiuh_$)fwi9rrpdutiv1pl#r6bf6verv*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
