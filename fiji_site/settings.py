9272d2c0f8b79fd5e0b33df57ad3216521f97f10
zelenove <31376761+zelenove@users.noreply.github.com>
2019-04-22 13:54:34 -0400

    Made secret key hidden for public repo

--- fiji_site/settings.py
+++ fiji_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9u=p%_2tri4f+i+^ldiuh_$)fwi9rrpdutiv1pl#r6bf6verv*'
+SECRET_KEY = 'NULL'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
